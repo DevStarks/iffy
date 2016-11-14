ae9d7eac34b8a418b890ed843c53b0c8e83adb44
Devin Starks <DevStarks@gmail.com>
2016-11-14 14:41:53 -0500

    adds package.json

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require("webpack");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/main.jsx",
+  output: {
+    path: "./app/assets/javascripts",
+    filename: "bundle.js"
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
